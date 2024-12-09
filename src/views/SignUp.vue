7338b78a8cb50b1aaae72b33f057360c5e09b3e5
Glêsio Santos <glesioss@gmail.com>
2024-12-09 16:57:25 -0300

    enviando dados do formulario

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -1,21 +1,25 @@
 <template>
   <h1>Sign In</h1>
-  <form action="">
+  <form @submit.prevent="handleSubmit">
     <div class="mb-3">
-      <label for="nameInput" class="form-label">Name</label>
-        <input type="text" class="form-control" id="nameInput" placeholder="João Albuquerque">
+      <label for="firstNameInput" class="form-label">First Name</label>
+        <input type="text" class="form-control" id="firstNameInput" v-model="form.first">
+    </div>
+    <div class="mb-3">
+      <label for="lastNameInput" class="form-label">Last Name</label>
+        <input type="text" class="form-control" id="lastNameInput" v-model="form.last">
     </div>
     <div class="mb-3">
       <label for="emailInput" class="form-label">E-mail</label>
-        <input type="email" class="form-control" id="emailInput" placeholder="name@example.com">
+        <input type="email" class="form-control" id="emailInput" v-model="form.email">
     </div>
     <div class="mb-3">
         <label for="passwordInput" class="form-label">Password</label>
-        <input type="password" class="form-control" id="passwordInput1" placeholder="exemplo.: 102030">
+        <input type="password" class="form-control" id="passwordInput1" v-model="form.password">
     </div>
     <div class="mb-3">
         <label for="passwordConfirmationInput" class="form-label">Password Confirmation</label>
-        <input type="password" class="form-control" id="passwordConfirmationInput1" placeholder="exemplo.: 102030">
+        <input type="password" class="form-control" id="passwordConfirmationInput1" v-model="form.passwordConfirmation">
     </div>
 
     <div class="d-grid gap-2">
@@ -23,4 +27,19 @@
     </div>
   </form>
 </template>
-<script setup></script>
+<script setup>
+import { ref } from 'vue';
+
+  let form = ref({
+    first: '',
+    last: '',
+    email: '',
+    password: '',
+    passwordConfirmation: ''
+  })
+
+  function handleSubmit () {
+    console.log(form);
+  }
+
+</script>
